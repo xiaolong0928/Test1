c9d3152cf09e8a348fedd7bb4da648d14a26d2ed
shenhw <shenhw@asiainfo.com>
2018-03-06 19:54:36 +0800

    first commit

---
+++ parcelDemo/index.js
@@ -0,0 +1,7 @@
+// 导入另一个组件
+import main from './main';
+
+main();
+/**
+ * Created by shenhw-pc on 2018/3/2.
+ */
---
+++ parcelDemo/main.js
@@ -0,0 +1,9 @@
+// 导入一个 CSS module
+import classes from './main.css';
+
+export default () => {
+    console.log(classes.main);
+};
+/**
+ * Created by shenhw-pc on 2018/3/2.
+ */
